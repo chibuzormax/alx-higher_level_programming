b4d7e435966fd039ae609b6fd95cbe703078d4f2
Maximus Isaac <chibuzormax@gmail.com>
2023-03-29 07:40:58 -0700

    magic_calculation

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 def magic_calculation(a, b):
     result = 0
 
@@ -12,4 +13,4 @@ def magic_calculation(a, b):
             result = b + a
             break
 
-    return result
+    return (result)
