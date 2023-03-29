2e77b2dad1497b39a8a58e0e9fc30f13291c7395
Maximus Isaac <chibuzormax@gmail.com>
2023-03-29 14:46:55 -0700

    5-square.py

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -3,14 +3,18 @@
 
 
 class Square:
-     """Represent a square."""
+    """Represent a square.
+    Attributes:
+        __size (int): size of a side of the square
+    """
 
     def __init__(self, size=0):
-          """Initialize a new square.
-
-          Args:
-          size (int): The size of the new square.
-          """
+        """Initialize a new square.
+        Args:
+            size (int): The size of the new square.
+        Returns:
+            None
+        """
         self.size = size
 
     def area(self):
@@ -22,12 +26,21 @@ class Square:
 
     @property
     def size(self):
-         """Get/set the current size of the square."""
+         """Get/set the current size of the square.
+         Returns:
+            Size of the square
+         """
          return (self.__size)
 
     @size.setter
     def size(self, value):
-        if not isinstance(value, int):
+        """setter of __size
+        Args:
+            value (int): size of a side of the square
+        Returns:
+            None
+        """
+        if type(value) is not int:
               raise TypeError("size must be an integer")
         elif value < 0:
               raise ValueError("size must be >= 0")
